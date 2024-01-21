d7ec158f4d8082afbb55478502921aaba689ad42
samtin0x <40127309+samtin0x@users.noreply.github.com>
2024-01-21 22:58:56 +0100

    Remove .idea folder

--- app/frontend/src/pages/index.tsx
+++ app/frontend/src/pages/index.tsx
@@ -1,34 +1,106 @@
-import React from 'react'
-import RootLayout from '../components/RootLayout'
-import Image from 'next/image'
+import * as React from 'react'
+import { Box, Grid, Paper, Typography } from '@mui/material'
+import '@/app/globals.css'
 
-const pageTitle: string = 'L2 Finality'
-const pageDescription: string = 'L2 Finality Description'
+import DropdownMenu from '@/components/ui/DropdownMenu'
+import RootLayout from '@/components/RootLayout'
+import AverageStats from '@/components/ui/AverageStats'
+import { averageStatsPaperStyle } from '@/styles/averageStatsStyles'
+import { dropdownCardPaperStyle } from '@/styles/dropdownCardStyles'
+import EconomicGuaranteesChart from '@/components/charts/EconomicGuaranteesChart'
+import AssetsAtRiskChart from '@/components/charts/AssetsAtRiskChart'
+import { graphPaperStyle } from '@/styles/graphCardStyles'
+import LastReorgsTable from '@/components/charts/LastReorgsTable'
 
-const HomePage: React.FC = () => {
+const pageTitle: string = 'L2 Finality Index'
+const pageDescription: string = 'Layer 2 finality dashboard description'
+
+const Index = () => {
   return (
     <RootLayout title={pageTitle} description={pageDescription}>
-      <div>
-        <h1 className="text-2xl font-bold text-blue-500">
-          Welcome to My Next.js App
-        </h1>
-        <p className="text-gray-700">
-          This is some text with Tailwind CSS styles.
-        </p>
-        <div className="z-10 max-w-5xl items-center justify-between font-mono text-sm">
-          <div className="h-48 flex w-full items-center justify-center lg:static lg:h-auto lg:w-auto lg:bg-none lgmb-12">
-            <Image
-              src={'/icons/base.png'}
-              alt="Base logo"
-              width={64}
-              height={72}
-            />
-            <p className="text-lg md:text-2xl ml-4">Base Logo</p>
-          </div>
-        </div>
-      </div>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          {/* Select chain Section */}
+          <Grid item xs={12}>
+            <Paper sx={dropdownCardPaperStyle}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  width: '100%',
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontWeight: 'bold',
+                    paddingLeft: '8px',
+                  }}
+                >
+                  Select chain
+                </Typography>
+                <DropdownMenu />
+              </Box>
+            </Paper>
+          </Grid>
+
+          {/* Average stats section */}
+          <Grid item xs={4} md={4}>
+            <Paper sx={averageStatsPaperStyle}>
+              <AverageStats
+                label="VaR"
+                prefix="$"
+                comparisonPeriod="week"
+                value={8902123}
+                percentageChange={-0.051345}
+              ></AverageStats>
+            </Paper>
+          </Grid>
+          <Grid item xs={4} md={4}>
+            <Paper sx={averageStatsPaperStyle}>
+              <AverageStats
+                label="Reorg frequency"
+                comparisonPeriod="week"
+                value={2}
+              ></AverageStats>
+            </Paper>
+          </Grid>
+          <Grid item xs={4} md={4}>
+            <Paper sx={averageStatsPaperStyle}>
+              <AverageStats
+                label="Monetary impact of reorgs"
+                prefix="$"
+                comparisonPeriod="month"
+                value={18902123}
+                percentageChange={0.02345}
+              ></AverageStats>
+            </Paper>
+          </Grid>
+
+          {/* Graphs Section */}
+          <Grid item xs={12}>
+            <Paper sx={graphPaperStyle}>
+              <Typography>Graph: value / time</Typography>
+              <EconomicGuaranteesChart />
+            </Paper>
+
+            <Paper sx={graphPaperStyle}>
+              <Typography>Graph: value / time</Typography>
+              <AssetsAtRiskChart />
+            </Paper>
+          </Grid>
+
+          {/* Table Section */}
+          <Grid item xs={12}>
+            <Paper sx={graphPaperStyle}>
+              <Typography>Table: Last reversions - value</Typography>
+              <LastReorgsTable />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Box>
     </RootLayout>
   )
 }
 
-export default HomePage
+export default Index
--- app/frontend/src/styles/graphCardStyles.ts
+++ app/frontend/src/styles/graphCardStyles.ts
@@ -6,5 +6,8 @@ export const graphPaperStyle: SxProps<Theme> = {
   backgroundColor: 'var(--light-background-color)',
   boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
   borderRadius: '8px',
-  maxWidth: '94%',
+  maxWidth: '1000px',
+  '@media (max-width: 1000px)': {
+    margin: '0 30px', // Adjust the margins for smaller screens
+  },
 }
