6c18bc3276a4ddf23341a589f58a150c718f25b2
samtin0x <40127309+samtin0x@users.noreply.github.com>
2024-02-20 20:04:36 +0100

    Add checksum validator to whitelisted asset json

--- app/backend/src/core/clients/ethereum/EthereumLogDecoder.test.ts
+++ app/backend/src/core/clients/ethereum/EthereumLogDecoder.test.ts
@@ -2,6 +2,8 @@ import { expect } from "earl";
 import contracts from "./contracts/contracts.json";
 import path from "path";
 import fs from "fs";
+import { ethers } from "ethers";
+import assets from "@/core/clients/coincap/assets/whitelisted.json";
 
 describe("ABI Files Check", () => {
   it("should have an ABI file for each contract in contracts.json", () => {
@@ -12,6 +14,19 @@ describe("ABI Files Check", () => {
       const abiFilePath = path.join(abiDirectory, `${contractName}.json`);
       const exists = fs.existsSync(abiFilePath);
       expect(exists).toEqual(true);
+    });
+  });
+
+  it("should have checksummed addresses for all assets", () => {
+    assets.forEach((asset) => {
+      try {
+        const checksummedAddress = ethers.getAddress(asset.address);
+        expect(checksummedAddress).toEqual(asset.address);
+      } catch (error) {
+        throw new Error(
+          `Address ${asset.address} for ${asset.name} (${asset.ticker}) is not checksummed.`,
+        );
+      }
     });
   });
 });
