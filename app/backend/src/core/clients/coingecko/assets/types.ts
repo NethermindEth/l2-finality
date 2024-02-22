06a6453c226e6a210e3c1b0b6e4b0ffe198e7308
Alex Bespalov <alexb5dh@gmail.com>
2024-02-22 12:12:33 +0200

    Use l2beat assets list

--- app/backend/src/core/clients/coingecko/assets/types.ts
+++ app/backend/src/core/clients/coingecko/assets/types.ts
@@ -1,17 +1,10 @@
-interface ContractDetails {
-  address: string;
-  decimals: number;
-}
-
-interface ContractAddresses {
-  [key: string]: ContractDetails;
-}
-
 export interface WhitelistedAsset {
+  name: string;
   coingeckoId: string;
+  address?: string;
+  symbol: string;
+  decimals: number;
   deploymentTimestamp: number;
   coingeckoListingTimestamp: number;
-  name: string;
-  ticker: string;
-  contracts: ContractAddresses;
+  chainId: number;
 }
