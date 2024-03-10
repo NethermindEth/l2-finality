7813b46ba7cba57de7061371685a9314ba6de64f
samtin0x <40127309+samtin0x@users.noreply.github.com>
2024-03-10 23:45:06 +0100

    Add latest block api endpoint

---
+++ app/frontend/src/api/blocksApi.ts
@@ -0,0 +1,12 @@
+import { ApiClient } from './ApiClient'
+import { LatestBlockViewModel } from '../../../shared/api/viewModels/BlocksEndpoint'
+
+const apiClient = new ApiClient()
+
+export const blocksApi = {
+  getLatestBlock(chainId: number): Promise<LatestBlockViewModel> {
+    return apiClient.get<LatestBlockViewModel>(
+      '/api/blocks/latest?chainId=' + chainId
+    )
+  },
+}
