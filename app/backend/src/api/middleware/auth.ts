b72399786d0c074484fce8a6ee0f9ecd3483b5dc
samtin0x <40127309+samtin0x@users.noreply.github.com>
2024-03-12 19:13:38 +0100

    Remove test logging

--- app/backend/src/api/middleware/auth.ts
+++ app/backend/src/api/middleware/auth.ts
@@ -9,7 +9,6 @@ export const authenticateApiKey = (config: Config) => {
       return sendErrorResponse(res, 401, "Unauthorized");
     }
     if (apiKey !== config.api.apiKey) {
-      console.log("apiKey", apiKey, "config.api.apiKey", config.api.apiKey);
       return sendErrorResponse(res, 403, "Forbidden");
     }
     next();
